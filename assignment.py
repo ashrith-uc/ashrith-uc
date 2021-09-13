fca86800a68f36a2d66ec762b2a81648c306b19c
ashrith-uc <90550355+ashrith-uc@users.noreply.github.com>
2021-09-13 18:51:08 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,23 @@
-# # read an integer i< n print i^2
-# # using loop
-# n = int(input("enter the number of elements"))
-# for i in range(n):
-#     print(i * i)
-# # making list as user input
-# n = int(input("enter the number of elements"))
-# a = list(map(int, input("Enter the numbers : ").split()))[:n]
-# # square of a numbers
-# print(list(map(lambda num: num * num, a)))
-#
-#
-#
-# # filter even numbers
-# print(list(filter(lambda num: num % 2 == 0, a)))
-# # filter odd numbers
-# print(list(filter(lambda num: num % 2 != 0, a)))
-# # filter age group above 18
-# print(list(filter(lambda num: num > 18, a)))
-# # prime numbers
-b = range(1, 100)
-for i in range(2, 3):
-    print(list(filter(lambda x: x % i or x == i, b)))
+# read an integer i< n print i^2
+# using loop
+n = int(input("enter the number of elements"))
+for i in range(n):
+    print(i * i)
+# making list as user input
+n = int(input("enter the number of elements"))
+a = list(map(int, input("Enter the numbers : ").split()))[:n]
+# square of a numbers
+print(list(map(lambda num: num * num, a)))
+
+
+
+# filter even numbers
+print(list(filter(lambda num: num % 2 == 0, a)))
+# filter odd numbers
+print(list(filter(lambda num: num % 2 != 0, a)))
+# filter age group above 18
+print(list(filter(lambda num: num > 18, a)))
+# prime numbers
 
 
 # zip
@@ -28,5 +25,3 @@ name = ('rohit', 'kohli', 'rahul', 'suryakumar yadav')
 runs = ('150', '128', '110', '88')
 avg = ("60.22", '80.11', '60.55', '40.12')
 print(list(zip(name, runs, avg)))
-
-
